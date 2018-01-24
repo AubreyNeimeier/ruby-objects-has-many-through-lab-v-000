2c716eeb6efa671ff5c634cb2697f2fbab4f8e85
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-24 18:53:01 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,6 +6,7 @@ August 1st"), and a doctor. The #initialize method should tell the new
 appointment that it belongs to the doctor passed in as an argument and tell
 the doctor that it now has this appointment in its collection of appointments.
 Use the Doctor#add_appointment method here.
+=end
 
 
 class Appointment
