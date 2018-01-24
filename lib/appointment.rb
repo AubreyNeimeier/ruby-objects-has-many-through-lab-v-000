92c263fbb39316c452bd6df82c3ad76a0d114291
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-24 18:52:03 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,13 @@
 #appts belong to a doctor
 #appts belong to a patient
+=begin
+An appointment should be initialized with a date (as a string, like "Monday,
+August 1st"), and a doctor. The #initialize method should tell the new
+appointment that it belongs to the doctor passed in as an argument and tell
+the doctor that it now has this appointment in its collection of appointments.
+Use the Doctor#add_appointment method here.
+
+
 class Appointment
   attr_accessor :doctor, :patient, :appointment
 
